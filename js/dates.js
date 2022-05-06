5d359221c077f55818f830c5766446190469a44b
Subrat Mallick <m.subrat198470@gmail.com>
2022-05-06 20:18:16 +0530

    x

--- js/dates.js
+++ js/dates.js
@@ -1,7 +1,7 @@
 {
-    const date1 = new Date('3/19/2022'); //month date year
-    const date2 = new Date();
-    const diffTime = Math.abs(date2 - date1);
+    const date11 = new Date('3/19/2022'); //month date year
+    const date12 = new Date();
+    const diffTime = Math.abs(date12 - date11);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     console.log(diffTime + " milliseconds");
     console.log(diffDays + " days");
@@ -9,9 +9,9 @@
 }
 
 {
-    const date11 = new Date('3/23/2022'); //month date year
-    const date21 = new Date();
-    const diffTime = Math.abs(date21 - date11);
+    const date21 = new Date('3/23/2022');
+    const date22 = new Date();
+    const diffTime = Math.abs(date22 - date21);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     console.log(diffTime + " milliseconds");
     console.log(diffDays + " days");
