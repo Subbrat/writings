5160128e6e92c9bc97af6662fad9462c4e27c859
Subbrat <m.subbrat@outlook.com>
2023-09-20 12:00:45 +0530

    Update randomlines.js

--- js/randomlines.js
+++ js/randomlines.js
@@ -2,7 +2,7 @@
 
     { //wr1
         const d1 = [
-            "No one could understand me better than you do, no one is there whom I could speak my heart out,nor I want to make any.",
+            "No one could understand me better than you do, no one is there whom I could speak my heart out, nor I want to make any.",
             "We both know these all are temporary chapters and will leave permanent marks and lessons, will leave us in pain.",
             "They speak to you because you are the most broken one they could ever see.",
             "No one wants to stay with you forever, You are not eligible.",
